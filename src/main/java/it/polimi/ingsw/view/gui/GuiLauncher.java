5bca365691e4a9eee80cb90f23ec8c37764b82fc
polimi-hyp-2018-10493663 <paolo1.boffi@mail.polimi.it>
2018-06-11 15:12:52 +0200

    Login screen added and connected to server. Default connection set to RMI connection, with possibility to switch to socket connection with checkbox "slow connection".

--- src/main/java/it/polimi/ingsw/view/gui/GuiLauncher.java
+++ src/main/java/it/polimi/ingsw/view/gui/GuiLauncher.java
@@ -1,46 +1,53 @@
 package it.polimi.ingsw.view.gui;
 
-import it.polimi.ingsw.connection.client.Client;
 import it.polimi.ingsw.connection.client.ConnectionController;
-import it.polimi.ingsw.view.gui.login.LoginController;
+import it.polimi.ingsw.connection.client.ConnectionType;
+import it.polimi.ingsw.view.gui.gameboard.GameBoardLauncher;
 import it.polimi.ingsw.view.gui.settings.GUIParameters;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 public class GuiLauncher extends Application {
 
     private Stage primaryStage;
+    private ConnectionController connectionController;
+    private ConnectionType connectionType = ConnectionType.RMI;
+    @FXML
+    private TextField username;
+    @FXML
+    private Label usernameNotValid;
 
-    public void chooseRMIConnection() throws Exception {
-        connectionHandler("RMI");
-    }
-    public void chooseSocketConnection() throws Exception {
-        connectionHandler("Socket");
-    }
-    private void connectionHandler(String typeOfConnection) throws Exception {
-        //Management of type of connection, sending information to server
-        setStage(new Stage());
-        new LoginController().start(primaryStage);
+    public void slowConnection(){
+        this.connectionType = ConnectionType.SOCKET;
     }
 
-    public void setStage(Stage stage){
-        this.primaryStage = stage;
+    public void getUsername() throws Exception {
+        checkAndSaveUsername(username.getText());
+    }
+    private void checkAndSaveUsername(String username) throws Exception {
+        connectionController = new ConnectionController(connectionType);
+        boolean isValid = connectionController.restore(username);
+        if (isValid)
+            new GameBoardLauncher().start(new Stage());
+        else
+            usernameNotValid.setText("Username not valid, insert a new one");
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        setStage(primaryStage);
-        Parent parent = FXMLLoader.load(getClass().getResource("/FXML_files/ConnectionChoice.fxml"));
-        primaryStage.setTitle(GUIParameters.CONNECTION_SCENE_TITLE);
+        this.primaryStage = primaryStage;
+        Parent parent = FXMLLoader.load(getClass().getResource("/FXML_files/ConnectionLogin.fxml"));
+        this.primaryStage.setTitle(GUIParameters.CONNECTION_SCENE_TITLE);
 
         Scene scene = new Scene(parent, GUIParameters.CONNECTION_LOGIN_SCENE_WIDTH, GUIParameters.CONNECTION_LOGIN_SCENE_HEIGHT);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    public static void main(String args[]){
-        launch(args);
+        this.primaryStage.setScene(scene);
+        this.primaryStage.show();
     }
 }
