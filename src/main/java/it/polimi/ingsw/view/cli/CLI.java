5da94d943e3a6c4c2e8bf7145103695ad7b1f719
archettialberto <alberto1.archetti@mail.polimi.it>
2018-07-05 12:18:43 +0200

    CLI default image for cards Added a default image for unknown cards by the Client. In this way the Server can release new cards without necessarily updating the client data.

--- src/main/java/it/polimi/ingsw/view/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/view/cli/CLI.java
@@ -53,7 +53,10 @@ public class CLI implements Runnable {
     private static final int ROUND_TRACK_LENGTH = 44;
     private static final int TOOL_IMAGE_LENGTH = 9;
     private static final int OBJECTIVE_IMAGE_LENGTH = 15;
-    private static final String CLI_IMAGES_PATH = "src/main/java/res/cliimages/card";
+    private static final String EXT = ".cliimage";
+    private static final String CLI_IMAGES_PATH = "src/main/java/res/cliimages/";
+    private static final String PRE_ID = "card";
+    private static final String DEFAULT_CLI_IMAGE = "default";
 
     private Scanner scanner;
     private ConnectionManager connectionController;
@@ -344,31 +347,43 @@ public class CLI implements Runnable {
     }
 
     private String drawCard(JSONObject jsonObject) {
+        int id = parseInt(jsonObject.get(JSONTag.CARD_ID).toString());
+        String image = loadImage(id);
+        String name = "[" + id + "] " + jsonObject.get(JSONTag.NAME).toString();
+        String description = jsonObject.get(JSONTag.DESCRIPTION).toString();
+        Long points = (Long) jsonObject.get(JSONTag.FAVOR_POINTS);
+        if (points != null)
+            name = name + " (FP:" + points.intValue() + ")";
+        StringBuilder sb = new StringBuilder();
+        String[] splitImage = image.split("\n");
+        int imageLength;
+        if (splitImage.length == 3) {
+            sb.append("|");
+            for (int i = 0; i < PIXEL_WIDTH; i++) sb.append(" ");
+            sb.append("|\n");
+            imageLength = TOOL_IMAGE_LENGTH;
+        } else imageLength = OBJECTIVE_IMAGE_LENGTH;
+        for (String line : splitImage) {
+            sb.append("|");
+            for (int i = 0; i < (PIXEL_WIDTH - imageLength) / 2; i++) sb.append(" ");
+            sb.append(line);
+            for (int i = 0; i < PIXEL_WIDTH - imageLength - (PIXEL_WIDTH - imageLength) / 2; i++) sb.append(" ");
+            sb.append("|\n");
+        }
+        return getUpperCard() + sb.toString() + getLowerCard(name, description);
+    }
+
+    private String loadImage(int id) {
         try {
-            int id = parseInt(jsonObject.get(JSONTag.CARD_ID).toString());
-            String image = new String(Files.readAllBytes(Paths.get(CLI_IMAGES_PATH + id + ".cliimage")));
-            String name = "[" + id + "] " + jsonObject.get(JSONTag.NAME).toString();
-            String description = jsonObject.get(JSONTag.DESCRIPTION).toString();
-            Long points = (Long) jsonObject.get(JSONTag.FAVOR_POINTS);
-            if (points != null)
-                name = name + " (FP:" + points.intValue() + ")";
-            StringBuilder sb = new StringBuilder();
-            String[] splitImage = image.split("\n");
-            int imageLength;
-            if (splitImage.length == 3) {
-                sb.append("|");
-                for (int i = 0; i < PIXEL_WIDTH; i++) sb.append(" ");
-                sb.append("|\n");
-                imageLength = TOOL_IMAGE_LENGTH;
-            } else imageLength = OBJECTIVE_IMAGE_LENGTH;
-            for (String line : splitImage) {
-                sb.append("|");
-                for (int i = 0; i < (PIXEL_WIDTH - imageLength) / 2; i++) sb.append(" ");
-                sb.append(line);
-                for (int i = 0; i < PIXEL_WIDTH - imageLength - (PIXEL_WIDTH - imageLength) / 2; i++) sb.append(" ");
-                sb.append("|\n");
-            }
-            return getUpperCard() + sb.toString() + getLowerCard(name, description);
+            return new String(Files.readAllBytes(Paths.get(CLI_IMAGES_PATH + PRE_ID + id + EXT)));
+        } catch (IOException e) {
+            return loadDefaultImage();
+        }
+    }
+
+    private String loadDefaultImage() {
+        try {
+            return new String(Files.readAllBytes(Paths.get(CLI_IMAGES_PATH + DEFAULT_CLI_IMAGE + EXT)));
         } catch (IOException e) {
             throw new IllegalArgumentException(IMAGE_NOT_FOUND);
         }
